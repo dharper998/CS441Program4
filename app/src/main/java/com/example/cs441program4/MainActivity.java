a1dfce4aac403bbf665ae6f837729c5ab1679a50
Drew Harper <dharper998@gmail.com>
2021-03-10 11:17:57 -0500

    Adding basic implementation of Recycler View. Plan to add Button to add/remove items from list

--- app/src/main/java/com/example/cs441program4/MainActivity.java
+++ app/src/main/java/com/example/cs441program4/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.cs441program4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        RecyclerView recyclerView = findViewById(R.id.rview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        CustomAdapter adapter = new CustomAdapter(animalNames);
+        recyclerView.setAdapter(adapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+
+    public void addItem(View view) {
+        
     }
 }
